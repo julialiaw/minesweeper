b324c29f8d726f935d2e0c201e49640c86cb3f7f
julialiaw <juliawux01@gmail.com>
2026-06-23 19:11:13 -0400

    added function to place mines throughout grid

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var grid = document.getElementById("grid");
+var testing = true;
 
 generateGrid();
 
@@ -11,6 +12,23 @@ function generateGrid() {
             cell.addEventListener("click", function(){
                 console.log("clicked");
             })
+            var mine = document.createAttribute("data-mine");
+            mine.value = "false";
+            cell.setAttributeNode(mine);
+        }
+    }
+    placeMines();
+}
+
+function placeMines() {
+    // place 10 mines randomly in the grid
+    for (var i = 0; i < 10; i++) {
+        var row = Math.floor(Math.random() * 10);
+        var col = Math.floor(Math.random() * 10);
+        var cell = grid.rows[row].cells[col];
+        cell.setAttribute("data-mine", "true");
+        if (testing) {
+            cell.innerHTML = "X";
         }
     }
 }
