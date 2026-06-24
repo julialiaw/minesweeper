8b8761f47c80a94af4a559bcb1fefb296a3c943f
julialiaw <juliawux01@gmail.com>
2026-06-24 11:42:34 -0400

    added flagging with right-click, polished cell reveals

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 var grid = document.getElementById("grid");
-var testing = true;
+var testing = false;
 var firstMove = true;
+var gameOver = false;
 
 generateGrid();
 
 function generateGrid() {
     // generate a 10x10 grid!!
     for (var i = 0; i < 10; i++) {
-        row = grid.insertRow(i);
+        var row = grid.insertRow(i);
         for (var j = 0; j < 10; j++) {
-            cell = row.insertCell(j);
-            cell.onclick = function() {
-                makeMove(this);
-            }
+            var cell = row.insertCell(j);
+            cell.setAttribute("state", "unclicked");
+            cell.addEventListener("mousedown", (event) => {
+                if (gameOver) return;
+                if (event.button == 0) {
+                    if (event.currentTarget.getAttribute("state") == "unclicked") {
+                        makeMove(event.currentTarget);
+                    }
+                }
+            })
+            cell.addEventListener("contextmenu", (event) => {
+                if (gameOver) return;
+                event.preventDefault();
+                if (event.currentTarget.getAttribute("state") != "clicked") {
+                    flagCell(event.currentTarget); 
+                }
+            })
             var mine = document.createAttribute("data-mine");
             mine.value = "false";
             cell.setAttributeNode(mine);
@@ -23,7 +37,7 @@ function generateGrid() {
 
 function placeMines() {
     // place 10 mines randomly in the grid
-    for (var i = 0; i < 10.5; i++) {
+    for (var i = 0; i < 15; i++) {
         var row = Math.floor(Math.random() * 10);
         var col = Math.floor(Math.random() * 10);
         var cell = grid.rows[row].cells[col];
@@ -35,53 +49,61 @@ function placeMines() {
 }
 
 function moveFirst(cell) {
-    if (firstMove == true && cell.getAttribute("data-mine") == "true") {
+    if (cell.getAttribute("data-mine") == "true") {
         cell.setAttribute("data-mine", "false");
         cell.classList.remove("mine");
         let i = false;
         while (i == false) {
             var row = Math.floor(Math.random() * 10);
             var col = Math.floor(Math.random() * 10);
-            var cell = grid.rows[row].cells[col];
-            if (cell.getAttribute("data-mine") == "false") {
-                cell.setAttribute("data-mine", "true")
+            var checkCell = grid.rows[row].cells[col];
+            if (checkCell.getAttribute("data-mine") == "false") {
+                checkCell.setAttribute("data-mine", "true")
                 i = true;
             }
         }
-        firstMove = false;
     }
 }
 
 function countMines(cell) {
-    
+    // count number of mines surrounding a cell
+    var cellRow = cell.parentNode.rowIndex;
+    var cellCol = cell.cellIndex;
+    var mineCount = 0;
+
+    // counting surrounding mines
+    for (var i = -1; i <= 1; i++) {
+        for (var j = -1; j <= 1; j++) {
+            if (i == 0 && j == 0) continue;
+            if (grid.rows[cellRow + i]) {
+                if (grid.rows[cellRow + i].cells[cellCol + j]) {
+                    var checkCell = grid.rows[cellRow + i].cells[cellCol + j];
+                    if (checkCell.getAttribute("data-mine") == "true") {
+                        mineCount += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    return mineCount;
 }
 
 function makeMove(cell) {
-    moveFirst(cell)
-    if (cell.className == "clicked") return;
+    if (firstMove == true) {
+        moveFirst(cell);
+        firstMove = false;
+    }
+    if (cell.getAttribute("state") == "clicked") return;
     if (cell.getAttribute("data-mine") == "true") {
         revealMines()
         alert("Game Over");
     } else {
         cell.className = "clicked";
+        cell.setAttribute("state", "clicked");
         var cellRow = cell.parentNode.rowIndex;
         var cellCol = cell.cellIndex;
-        var mineCount = 0;
-
-        // counting surrounding mines
-        for (var i = -1; i <= 1; i++) {
-            for (var j = -1; j <= 1; j++) {
-                if (i == 0 && j == 0) continue;
-                if (grid.rows[cellRow + i]) {
-                    if (grid.rows[cellRow + i].cells[cellCol + j]) {
-                        var checkCell = grid.rows[cellRow + i].cells[cellCol + j];
-                        if (checkCell.getAttribute("data-mine") == "true") {
-                            mineCount += 1;
-                        }
-                    }
-                }
-            }
-        }
+        var mineCount = countMines(cell);
 
         cell.innerHTML = mineCount;
 
@@ -93,7 +115,7 @@ function makeMove(cell) {
                     if (grid.rows[cellRow + i]) {
                         if (grid.rows[cellRow + i].cells[cellCol + j]) {
                             var checkCell = grid.rows[cellRow + i].cells[cellCol + j];
-                            if (checkCell.className !== "clicked") {
+                            if (checkCell.getAttribute("state") == "unclicked") {
                                 makeMove(checkCell);
                             }
                         }
@@ -113,15 +135,38 @@ function revealMines() {
     for (var i=0; i < 10; i++) {
         for (var j=0; j < 10; j++) {
             var cell = grid.rows[i].cells[j];
-            if (cell.getAttribute("data-mine") =="true" && cell.className != "mine") {
-                cell.className="mine"
-                var img = document.createElement('img');
-                img.src = "cute bomb.png";
-                img.style.width="30px";
-                img.style.width="30px";
-                cell.appendChild(img);
+            if (cell.getAttribute("data-mine") =="true" && cell.className != "mine" && cell.getAttribute("state") == "unclicked") {
+                cell.className="mine";
+                var bomb = document.createElement('img');
+                bomb.id = 'bomb';
+                bomb.src = "cute bomb.png";
+                bomb.style.height="30px";
+                bomb.style.width="30px";
+                cell.appendChild(bomb);
             }
+            else if (cell.getAttribute("state") == "false flag") {
+                cell.className = "mine";
+            }
+            
         }
     }
     
+}
+
+// for flag: cell.removeChild(flag)
+function flagCell(cell) {
+    var flag = document.createElement('img');
+    flag.id = 'flag';
+    flag.src = "simple flag.png";
+    flag.style.width="30px";
+    flag.style.width="30px";
+    if (cell.getAttribute("state") == "unclicked") {
+        if (cell.getAttribute("data-mine") == "false") {
+            cell.setAttribute("state", "false flag")
+        } else { cell.setAttribute("state", "flag") }
+        cell.appendChild(flag);
+    } else {
+        cell.setAttribute("state", "unclicked")
+        cell.innerHTML = "";
+    }
 }
